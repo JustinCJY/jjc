858a84f6be74a60faedde5627354e509ce0950c0
Justin <a0928347556@gmail.com>
2025-07-23 15:40:44 +0800

    777

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World!")
+0.5
+1
+    ; ("use strict")
+    ; ("jgjgjgjgjg")
+true
+false
+let number = 2
+const name = "Justin"
+number = 3
+temp = number
+console.log(temp)
+number = 4
+temp = number
+console.log(temp)
+let a = 10
+let b = 20
+console.log(a == 10)
+console.log(a != 10)
+console.log(a == 10 && b == 20)
+console.log(a == 10 || b == 10)
+console.log(a <= 10 && b >= 20)
